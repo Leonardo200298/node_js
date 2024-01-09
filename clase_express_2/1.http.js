cf02077e0e59650f021704a200018443c4a315cc
Leonardo <leonardosanchez119@gmail.com>
2024-01-09 15:13:35 -0300

    creacion de servidor simple

--- clase_express_2/1.http.js
+++ clase_express_2/1.http.js
@@ -3,10 +3,22 @@ const http = require('node:http')
 
 const desiredPort = process.env.PORT ?? 1234;
 
-const server = http.createServer((req, res) => {
-    console.log('request receivied', req.url);
-    res.end('hola mundo')
-})
+const processRequest = (req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    if (req.url === '/') {
+        res.statusCode = 200;
+        res.end('<h1>Bienvenido a mi página de inicio</h1>')
+    }else if (req.url === '/prueba'){
+        res.statusCode = 200;
+        res.end('<h3>Estas en la seccion prueba</h3>')
+    }else{
+        res.statusCode = 404;
+        res.end('Ruta no encontrada')
+    }
+}
+
+
+const server = http.createServer(processRequest)
 
 server.listen(desiredPort, () => {
     console.log(`Escuchando en el puerto http://localhost:${desiredPort}`);
